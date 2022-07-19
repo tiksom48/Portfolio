148760320f6e3d67ad7ac80f7ba837db1aa72ccb
Jonathan Patrich <jonathanpatrich@outlook.com>
2022-07-19 19:07:34 -0300

    fix::d

--- routes/server.js
+++ routes/server.js
@@ -2,9 +2,11 @@ const express = require("express")
 const app = express()
 const PORT = 3000
 
+
 app.get("/",(rec,res)=>{
     res.json({message:"ok"})
 })
+
 
 app.listen(PORT,(rec,res)=>{
     console.log(`servidor iniciado na porta http://localhost:${PORT}`)
